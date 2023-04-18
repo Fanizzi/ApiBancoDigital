9761c208b4f7fd8b8c1aa63354b7bb839159a829
Lorenzo Fanizzi <fanizzicontato@gmail.com>
2023-04-18 15:56:42 -0300

    add Rotas

--- rotas.php
+++ rotas.php
@@ -1,10 +1,29 @@
 <?php
 
 use App\Controller\CorrentistaController;
+use App\Controller\ContaController;
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 switch ($uri)
 {
-    
+    case '/correntista/save':
+        CorrentistaController::save();
+    break;
+
+    case '/correntista/entrar':
+        CorrentistaController::entrar();
+    break;
+
+    case '/conta/pix/enviar':
+        ContaController::enviar();
+    break;
+
+    case '/conta/pix/receber':
+        ContaController::receber();
+    break;
+
+    case '/conta/extrato':
+        ContaController::extrato();
+    break;
 }
