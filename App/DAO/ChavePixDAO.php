3041b18459525d3f267a8afade67510dc083dadb
Lorenzo Fanizzi <fanizzicontato@gmail.com>
2023-08-08 14:20:13 -0300

    Alteração ChavePixDAO

--- App/DAO/ChavePixDAO.php
+++ App/DAO/ChavePixDAO.php
@@ -3,6 +3,7 @@
 namespace App\DAO;
 
 use App\Model\ChavePixModel;
+use PDO;
 
 class ChavePixDAO extends DAO
 {
@@ -11,47 +12,65 @@ class ChavePixDAO extends DAO
         parent::__construct();
     }
 
-    public function select()
+    public function save(ChavePixModel $model) : ?ChavePixModel
     {
-        $sql = "SELECT * FROM Chave_Pix";
-
-        $stmt = $this->conexao->prepare($sql);
-        $stmt->execute();
-
-        return $stmt->fetchAll(DAO::FETCH_CLASS);
+        return ($model->id == null) ? $this->insert($model) : $this->update($model);
     }
 
-    public function insert(ChavePixModel $p) : bool
+    public function insert(ChavePixModel $model) : ?ChavePixModel
     {
         $sql = "INSERT INTO Chave_Pix (chave, tipo, id_conta)
                 VALUES (?, ?, ?)";
         
         $stmt = $this->conexao->prepare($sql);
-        $stmt->bindValue(1, $p->chave,);
-        $stmt->bindValue(2, $p->tipo);
-        $stmt->bindValue(3, $p->id_conta);
+        $stmt->bindValue(1, $model->chave,);
+        $stmt->bindValue(2, $model->tipo);
+        $stmt->bindValue(3, $model->id_conta);
 
         return $stmt->execute();
+        
+        $model->id = $this->conexao->lastInsertId();
+
+        return $model;
     }
 
-    public function update(ChavePixModel $p) : bool
+    public function update(ChavePixModel $model) : ?ChavePixModel
     {
         $sql = "UPDATE Chave_Pix SET chave=?, tipo=?, id_conta=? WHERE id=?";
 
         $stmt = $this->conexao->prepare($sql);
-        $stmt->bindValue(1, $p->chave,);
-        $stmt->bindValue(2, $p->tipo);
-        $stmt->bindValue(3, $p->id_conta);
+        $stmt->bindValue(1, $model->chave,);
+        $stmt->bindValue(2, $model->tipo);
+        $stmt->bindValue(3, $model->id_conta);
+        $stmt->bindValue(4, $model->id);
 
-        return $stmt->execute();
+        $stmt->execute();
+
+        $model->id = $this->conexao->lastInsertId();
+
+        return $model;
     }
 
-    public function delete(int $id) : bool
+    public function select(int $id_correntista)
     {
-        $sql = "DELETE FROM Chave_Pix WHERE id = ?";
+        $sql = "SELECT cp.* FROM Chave_Pix cp
+                JOIN conta c ON (c.id_conta = cp.id_conta)
+                WHERE c.id_correntista = ? ";
 
         $stmt = $this->conexao->prepare($sql);
-        $stmt->bindValue(1, $id);
+        $stmt->bindValue(1, $id_correntista);
+        $stmt->execute();
+
+        return $stmt->fetchAll(PDO::FETCH_CLASS);
+    }
+
+    public function delete(ChavePixModel $model) : bool
+    {
+        $sql = "DELETE FROM Chave_Pix WHERE id = ? AND id_conta=? ";
+
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindValue(1, $model->id);
+        $stmt->bindValue(1, $model->id_conta);
         return $stmt->execute();
     }
 }
